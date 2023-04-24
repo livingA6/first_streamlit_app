d83c9723ec8bd56a1ea38417cefdb4f2a50e7cf5
Tomi Asikainen <57344483+livingA6@users.noreply.github.com>
2023-04-24 15:56:07 +0300

    Update streamlit_app.py
    made some corrections

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 streamlit.header( 'Breakfast Favorites")
-streamlit.text(' Omega 3 & Blueberry Oatmeal') streamlit. text ('03 Kale, Spinach & Rocket Smoothie') streamlit.text ('« Hard-Boiled Free-Range Egg')
-streamlit.text('S
-Avocado Toast')
+streamlit.text(' Omega 3 & Blueberry Oatmeal') 
+streamlit. text ('03 Kale, Spinach & Rocket Smoothie') 
+streamlit.text ('« Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
